196a257e17233f9eebdfa899e00fbffe232c9d48
Simon C Blyth <simoncblyth@gmail.com>
2025-03-15 16:51:44 +0800

    change rl9 to el9 following RHEL standard

--- docker-mock-gitlab-ci.sh
+++ docker-mock-gitlab-ci.sh
@@ -3,6 +3,16 @@ usage(){ cat << EOU
 docker-mock-gitlab-ci.sh:usage
 ===============================
 
+TLDR::
+
+    ~/sandbox/docker-mock-gitlab-ci.sh run
+
+    cd ~/junosw && sudo rm -rf build InstallArea  ## for clean build test 
+
+    ~/sandbox/docker-mock-gitlab-ci.sh exec
+
+
+
 Unfortunately "gitlab-runner exec" functionality for local workflow testing 
 is removed from gitlab-runner 16.0::
 
@@ -35,10 +45,20 @@ Usage:
 * in one session start the base container with docker_run function
 
 ~/sandbox/docker-mock-gitlab-ci.sh run
-    start container using "docker run", its 
-    interactive to allow checking the build in progress 
+    start container using "docker run", it is interactive 
+    to allow checking the build in progress 
+
+GPU=1 ~/sandbox/docker-mock-gitlab-ci.sh run
+    setting GPU gives access within the container to the hosts GPU 
+    as provided by nvidia_container_toolkit package
+    This is needed for testing, but not building. 
 
 * in another session run the payload within the container with docker_exec function
+
+To force full build on host::
+
+   cd ~/junosw
+   sudo rm -rf build InstallArea
 
 ~/sandbox/docker-mock-gitlab-ci.sh exec
     performs the below *implicit* and *script* functions within the container started above. 
@@ -55,6 +75,16 @@ The kind of changes to be tested:
 2. doing junosw+opticks build by setting OPTICKS_PREFIX to point
    to an Opticks release on /cvmfs/opticks.ihep.ac.cn/  
 
+
+Issues
+--------
+
+1. currently are chown changing host user/group of junosw to juno:juno in implicit_bef
+   and changing back to again in implicit_aft 
+
+   * thats ugly, better way ? 
+
+
 EOU
 }
 
@@ -66,28 +96,33 @@ docker-mock-gitlab-ci.sh:info
    docker_ref : $(docker_ref)
    docker_nam : $(docker_nam)
 
-
-+--------------+-----------+----------------------+
-| nam          | size      |  notes               |
-+==============+===========+======================+
-| base         |   2.51GB  |  no CUDA             |
-+--------------+-----------+----------------------+
-| runtime      |   5.81GB  |  misses headers      |
-+--------------+-----------+----------------------+
-| runtimeplus  |   8.88GB  |  cherrypick devel    |      
-+--------------+-----------+----------------------+
-| devel        |  >10GB ?  |                      | 
-+--------------+-----------+----------------------+
-
+   docker_opticks_prefix : $(docker_opticks_prefix)   
+      ## non /cvmfs for debug only 
 
 EOI
 }
 
+notes(){ cat << EON
+
++--------------+-----------+------------------------------------+
+| nam          | size      |  notes                             |
++==============+===========+====================================+
+| base         |   2.51GB  |  no CUDA                           |
++--------------+-----------+------------------------------------+
+| runtime      |   5.81GB  |  misses headers                    |
++--------------+-----------+------------------------------------+
+| runtimeplus  |   7.5GB   |  cherrypick devel                  |      
++--------------+-----------+------------------------------------+
+| devel        |  10GB+?   |  might be too big for GHA VM  ?    | 
++--------------+-----------+------------------------------------+
+
+
+
+EON
+}
 
 defarg=script
 arg=${1:-$defarg}
-
-
 
 #docker_nam(){ echo base ; }           
 #docker_nam(){ echo runtime ; }       
@@ -97,9 +132,18 @@ docker_nam(){ echo runtimeplus ; }
 docker_ref(){  echo junosw/cuda:12.4.1-$(docker_nam)-rockylinux9 ; }
 
 
+#docker_opticks_prefix(){ echo /cvmfs/opticks.ihep.ac.cn/ok/releases/Opticks-v0.2.1/x86_64-CentOS7-gcc1120-geant4_10_04_p02-dbg ; }
+#docker_opticks_prefix(){ echo /cvmfs/opticks.ihep.ac.cn/ok/releases/Opticks-v0.3.1/x86_64--gcc11-geant4_10_04_p02-dbg ; }
+docker_opticks_prefix(){ echo /data1/blyth/local/opticks_Debug/Opticks-v0.3.1/x86_64--gcc11-geant4_10_04_p02-dbg ; }   ## for faster cycle debug
+
+
 docker_run_opts(){ 
+
+   : building should not need GPU access but testing will
    local ref=$(docker_ref)
-   if [ "${ref/cuda}" != "$ref" ]; then
+
+   #if [ "${ref/cuda}" != "$ref" ]; then
+   if [ -n "$GPU" ]; then
       cat << EOO
          --runtime=nvidia --gpus=all
 EOO
@@ -109,15 +153,30 @@ EOO
 }
 
 
+docker_run_notes(){ cat << EON
+
+      --mount type=bind,source=$dop,target=$dop,ro \
+
+EON
+}
+
 docker_run()
 {
    : docker_run - starting container with mounts from host into container 
    type $FUNCNAME
+
+   info
+
+   local dop=$(docker_opticks_prefix)  
+   : dop mount only needed for fast cycle debug
+
    docker run --rm -it \
       $(docker_run_opts) \
       --name $(docker_nam) \
-      --mount type=bind,source=/cvmfs,target=/cvmfs,ro \
+      --mount type=bind,source=/cvmfs/juno.ihep.ac.cn,target=/cvmfs/juno.ihep.ac.cn,ro \
+      --mount type=bind,source=/cvmfs/opticks.ihep.ac.cn,target=/cvmfs/opticks.ihep.ac.cn,ro \
       --mount type=bind,source=$HOME/junosw,target=/home/juno/junosw \
+      --volume $dop:$dop \
       $(docker_ref) 
 
    docker ps -a
@@ -130,29 +189,50 @@ docker_exec()
    docker exec -i $(docker_nam) bash < $BASH_SOURCE  
 }
 
-implicit()
+implicit_bef()
 {
     : implicitly done by gitlab-ci
 
+    type $FUNCNAME
     pwd
     sudo chown -R juno:juno junosw
     cd junosw       ## thats where the .gitlab-ci.yml is
     pwd
 }
+implicit_aft()
+{
+    local uid=$(id -nu)
+    local gid=$(id -ng)
+    cd  
+    sudo chown -R $uid:$gid junosw
+}
 
 script()
 {
     : equivalent to the script block of junosw/.gitlab-ci.yml
+    type $FUNCNAME
 
-    sudo mount -t cvmfs juno.ihep.ac.cn /cvmfs/juno.ihep.ac.cn
+    #sudo mount -t cvmfs juno.ihep.ac.cn /cvmfs/juno.ihep.ac.cn
+    #sudo mount -t cvmfs opticks.ihep.ac.cn /cvmfs/opticks.ihep.ac.cn
+    export OPTICKS_PREFIX=$(docker_opticks_prefix)
+
     #export JUNOTOP=/cvmfs/juno.ihep.ac.cn/el9_amd64_gcc11/Release/Jlatest
     export JUNOTOP=/cvmfs/juno.ihep.ac.cn/el9_amd64_gcc11/Release/J25.2.3
     export JUNO_CLANG_PREFIX
+
     export EXTRA_BUILD_FLAGS
     export JUNO_OFFLINE_OFF=1
     env | grep JUNO
     source $JUNOTOP/setup.sh
     if [ -n "$JUNO_CLANG_PREFIX" ]; then source $JUNO_CLANG_PREFIX/bashrc; fi
+    if [ -n "$OPTICKS_PREFIX" ]; then 
+         echo [ source $OPTICKS_PREFIX/bashrc
+         source $OPTICKS_PREFIX/bashrc
+         echo ] source $OPTICKS_PREFIX/bashrc
+    else
+         echo NOT with OPTICKS_PREFIX
+    fi
+
     env $EXTRA_BUILD_FLAGS ./build.sh
 }
 
@@ -161,6 +241,6 @@ case $arg in
   info) info ;;
   run) docker_run ;;
   exec) docker_exec ;;
-  script) implicit ; script ;; 
+  script) implicit_bef ; script ; implicit_aft  ;; 
 esac
 
