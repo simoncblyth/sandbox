541a6030060f4d1b269bfe78359577d87393798d
Simon C Blyth <simoncblyth@gmail.com>
2026-06-15 15:01:44 +0800

    getting Dockerfile ready to push the new JUNOSW+Opticks CUDA 13.1 image to hub

--- docker-mock-gitlab-ci.sh
+++ docker-mock-gitlab-ci.sh
@@ -3,15 +3,66 @@ usage(){ cat << EOU
 docker-mock-gitlab-ci.sh:usage
 ===============================
 
+NB the docker image tested here is created by local/build.sh
+
 TLDR::
 
+    ~/sandbox/docker-mock-gitlab-ci.sh run    ## SPIN UP THE CONTAINER
+    ~/sandbox/docker-mock-gitlab-ci.sh clean  ## REMOVE LOCAL DIRS ~/junosw/build/\$branch ~/junosw/InstallArea/\$branch
+    ~/sandbox/docker-mock-gitlab-ci.sh exec   ## RUN THE MOCK GITLAB-CI BUILD IN THE CONTAINER
+
+
+Usage
+-------
+
+This script allows JUNOSW+Opticks docker images/containers to be tested BEFORE
+pushing them to hub.docker.com and using them from gitlab-ci
+
+This script allows candidate modifications to the CI workflow to be tested
+without needing to change ~/junosw/.gitlab-ci.yml and without needing
+lots of pushes to trigger CI builds.
+
+The kind of changes to be tested:
+
+1. changing the base docker image to one that includes CUDA
+2. doing junosw+opticks build by setting OPTICKS_PREFIX to point
+   to an Opticks release on /cvmfs/opticks.ihep.ac.cn/
+
+
+Details
+--------
+
+More specifically this script provides a way to test a build using the environment created
+from a docker image to provide externals but using local directories::
+
+   ~/junosw
+   ~/junosw/build/$branch
+   ~/junosw/InstallArea/$branch
+
+
+
+Start base container using "docker run"::
+
     ~/sandbox/docker-mock-gitlab-ci.sh run
+    GPU=1 ~/sandbox/docker-mock-gitlab-ci.sh run
 
-    cd ~/junosw && sudo rm -rf build InstallArea  ## for clean build test
+Setting GPU gives access within the container to the hosts GPU
+as provided by nvidia_container_toolkit package
+This is needed for testing, but not building.
 
-    ~/sandbox/docker-mock-gitlab-ci.sh exec
+To force full(and slow) build do a clean::
+
+   ~/sandbox/docker-mock-gitlab-ci.sh clean
+
+Invoke the build via "docker exec" with::
+
+   ~/sandbox/docker-mock-gitlab-ci.sh exec
 
 
+
+
+About former "gitlab-runner exec" functionality that this script mimicks
+--------------------------------------------------------------------------
 
 Unfortunately "gitlab-runner exec" functionality for local workflow testing
 is removed from gitlab-runner 16.0::
@@ -36,54 +87,6 @@ is removed from gitlab-runner 16.0::
 
   "Deprecation of the gitlab-runner exec command from GitLab Runner"
 
-
-This script provides a manual workaround to do similar
-to the old "gitlab-runner exec".
-
-Usage:
-
-* in one session start the base container with docker_run function
-
-~/sandbox/docker-mock-gitlab-ci.sh run
-    start container using "docker run", it is interactive
-    to allow checking the build in progress
-
-GPU=1 ~/sandbox/docker-mock-gitlab-ci.sh run
-    setting GPU gives access within the container to the hosts GPU
-    as provided by nvidia_container_toolkit package
-    This is needed for testing, but not building.
-
-* in another session run the payload within the container with docker_exec function
-
-To force full build on host::
-
-   cd ~/junosw
-   sudo rm -rf build InstallArea
-
-~/sandbox/docker-mock-gitlab-ci.sh exec
-    performs the below *implicit* and *script* functions within the container started above.
-    The payload function invokes the junosw build.sh script
-
-
-This script allows candidate modifications to the CI workflow to be tested
-without needing to change junosw/.gitlab-ci.yml and without needing
-lots of pushes to trigger CI builds.
-
-The kind of changes to be tested:
-
-1. changing the base docker image to one that includes CUDA
-2. doing junosw+opticks build by setting OPTICKS_PREFIX to point
-   to an Opticks release on /cvmfs/opticks.ihep.ac.cn/
-
-
-Issues
---------
-
-1. currently are chown changing host user/group of junosw to juno:juno in implicit_bef
-   and changing back to again in implicit_aft
-
-   * thats ugly, better way ? create juno user on workstation for such tests.
-   * HMM actually for crontab gitlab-runner usage on workstation using juno user would be good also
 
 
 EOU
@@ -127,19 +130,26 @@ arg=${1:-$defarg}
 
 #docker_nam(){ echo base ; }
 #docker_nam(){ echo runtime ; }
-docker_nam(){ echo runtimeplus ; }
+#docker_nam(){ echo runtimeplus ; }
 #docker_nam(){ echo devel ; }
 
+docker_nam(){  echo from-junosw-test ; }
+
+
 #docker_ref(){  echo junosw/cuda:12.4.1-$(docker_nam)-rockylinux9 ; }
-docker_ref(){  echo simoncblyth/cuda:12.4.1-$(docker_nam)-rockylinux9 ; }
+
+#docker_ref(){  echo simoncblyth/cuda:12.4.1-$(docker_nam)-rockylinux9 ; }
+docker_ref(){  echo simoncblyth/junosw:el9.4-cuda13.1-opticks ; }
 
 
 #docker_opticks_prefix(){ echo /cvmfs/opticks.ihep.ac.cn/ok/releases/Opticks-v0.2.1/x86_64-CentOS7-gcc1120-geant4_10_04_p02-dbg ; }
 #docker_opticks_prefix(){ echo /cvmfs/opticks.ihep.ac.cn/ok/releases/Opticks-v0.3.1/x86_64--gcc11-geant4_10_04_p02-dbg ; }
 #docker_opticks_prefix(){ echo /data1/blyth/local/opticks_Debug/Opticks-v0.3.1/x86_64--gcc11-geant4_10_04_p02-dbg ; }
-docker_opticks_prefix(){ echo /data1/blyth/local/opticks_Debug/Opticks-v0.3.3/x86_64--gcc11-geant4_10_04_p02-dbg ; }
+#docker_opticks_prefix(){ echo /data1/blyth/local/opticks_Debug/Opticks-v0.3.3/x86_64--gcc11-geant4_10_04_p02-dbg ; }
+docker_opticks_prefix(){  echo /cvmfs/opticks.ihep.ac.cn/ok/releases/el9_amd64_gcc11/Opticks-v0.6.5 ; }
 
 docker_opticks_prefix_notes(){ cat << EON
+OPTICKS_PREFIX configures which Opticks to use for the JUNOSW+Opticks build being tested
 
 * Standard gitlab CI/CD uses OPTICKS_PREFIX from /cvmfs/opticks.ihep.ac.cn releases
 * For local workstation tests it is convenient to mount a local path avoiding
@@ -150,13 +160,9 @@ EON
 }
 
 
-
 docker_run_opts(){
 
    : building should not need GPU access but testing will
-   local ref=$(docker_ref)
-
-   #if [ "${ref/cuda}" != "$ref" ]; then
    if [ -n "$GPU" ]; then
       cat << EOO
          --runtime=nvidia --gpus=all
@@ -169,14 +175,19 @@ EOO
 
 docker_run_notes(){ cat << EON
 
-      --mount type=bind,source=$dop,target=$dop,ro \
+The eg "--user 1002:1002" option enables the user from the host to be effectively used
+from inside the container.  Without this it was necessary to "sudo chmod" to change ownership
+of files on host written by container processes.
+But a side effect is that the host user is likely unknown inside the container leading to
+the HOME envvar being set to "/" - that tickles a geant4-config bug resulting in broken prefix
+path.
 
 EON
 }
 
 docker_run()
 {
-   : docker_run - starting container with mounts from host into container 
+   : docker_run - starting container with mounts from host into container
    type $FUNCNAME
 
    info
@@ -184,61 +195,73 @@ docker_run()
    local dop=$(docker_opticks_prefix)
    : dop docker_opticks_prefix mount only needed for fast cycle debug
 
+   local host_user_id="$(id -u):$(id -g)"
+
    docker run --rm -it \
       $(docker_run_opts) \
       --name $(docker_nam) \
-      --mount type=bind,source=/cvmfs/juno.ihep.ac.cn,target=/cvmfs/juno.ihep.ac.cn,ro \
-      --mount type=bind,source=/cvmfs/opticks.ihep.ac.cn,target=/cvmfs/opticks.ihep.ac.cn,ro \
-      --mount type=bind,source=$HOME/junosw,target=/home/juno/junosw \
-      --volume $dop:$dop \
+      --user "$host_user_id" \
+      -v /cvmfs/juno.ihep.ac.cn:/cvmfs/juno.ihep.ac.cn:ro \
+      -v /cvmfs/opticks.ihep.ac.cn:/cvmfs/opticks.ihep.ac.cn:ro \
+      -v $HOME/junosw:/junosw \
+      -v $dop:$dop \
       $(docker_ref)
 
    docker ps -a
 }
 docker_exec()
 {
-   : docker_exec this script with defarg script doing implicit+script with bash in the container
+   : docker_exec this script with defarg script which invokes the below function within the container
    type $FUNCNAME
    docker ps -a
    docker exec -i $(docker_nam) bash < $BASH_SOURCE
 }
 
-implicit_bef()
-{
-    : implicitly done by gitlab-ci
 
-    type $FUNCNAME
-    pwd
-    sudo chown -R juno:juno junosw
-    cd junosw       ## thats where the .gitlab-ci.yml is
-    pwd
-}
-implicit_aft()
+
+
+
+
+
+
+local_clean()
 {
-    cd
-    #sudo chown -R blyth:blyth junosw
-    # invalid user within container
+    : use-for-clean-build-test-caution-slow
+    cd ~/junosw
+    local branch=$(git branch --show-current)
+    if [ -n "$branch" ]; then
+        if [ -d "build/$branch" ]; then
+            echo $FUNCNAME - removing build/$branch
+            rm -rf build/$branch
+        fi
+        if [ -d "InstallArea/$branch" ]; then
+            echo $FUNCNAME - removing build/$branch
+            rm -rf InstallArea/$branch
+        fi
+    fi
 }
 
-script()
+local_script()
 {
     : equivalent to the script block of junosw/.gitlab-ci.yml
     type $FUNCNAME
 
-    #sudo mount -t cvmfs juno.ihep.ac.cn /cvmfs/juno.ihep.ac.cn
-    #sudo mount -t cvmfs opticks.ihep.ac.cn /cvmfs/opticks.ihep.ac.cn
-    export JUNO_OPTICKS_PREFIX=$(docker_opticks_prefix)
+    export HOME=/junosw  # HOME being "/" messes up geant4-config path mechanics
+    export JUNO_OFFLINE_BUILD_BRANCH=1  ## use build/$branch InstallArea/$branch
 
-    #export JUNOTOP=/cvmfs/juno.ihep.ac.cn/el9_amd64_gcc11/Release/Jlatest
-    export JUNOTOP=/cvmfs/juno.ihep.ac.cn/el9_amd64_gcc11/Release/J25.2.3
+    export JUNO_OPTICKS_PREFIX=$(docker_opticks_prefix)
+    export JUNOTOP=/cvmfs/juno.ihep.ac.cn/el9_amd64_gcc11/Release/Jlatest
+
     export JUNO_CLANG_PREFIX
 
-    export EXTRA_BUILD_FLAGS
+    export EXTRA_BUILD_FLAGS="JUNO_CMAKE_BUILD_TYPE=Debug"
     export JUNO_OFFLINE_OFF=1
     env | grep JUNO
     source $JUNOTOP/setup.sh
     if [ -n "$JUNO_CLANG_PREFIX" ]; then source $JUNO_CLANG_PREFIX/bashrc; fi
     if [ -n "$JUNO_OPTICKS_PREFIX" ]; then source $JUNO_OPTICKS_PREFIX/bashrc ; fi
+
+    cd /junosw
     env $EXTRA_BUILD_FLAGS ./build.sh
 }
 
@@ -247,6 +270,7 @@ case $arg in
   info) info ;;
   run) docker_run ;;
   exec) docker_exec ;;
-  script) implicit_bef ; script ; implicit_aft  ;;
+  clean) local_clean ;;
+  script) local_script ;;
 esac
 
--- local/build.sh
+++ local/build.sh
@@ -1,15 +1,55 @@
 #!/bin/bash
 
+usage(){ cat << EOU
+build.sh
+==========
+
+::
+
+    ~/sandbox/local/build.sh info
+
+
+
+* https://hub.docker.com/r/junosw/base/tags
+
+
+hub.docker.com handles::
+
+    junosw/base:el9
+    junosw/base:el9-9.4
+
+    simoncblyth/junosw:el9.4-cuda13.1.2-opticks
+
+
+EOU
+}
+
 cd $(dirname $(realpath $BASH_SOURCE))
 
-NAM=simoncblyth
-DOK=junosw/Dockerfile-junosw-cudaxx-runtimeplus-el9
-DOKP=$(realpath ../$DOK)
-VERSION=$(sed -n 's/^ARG CUDA_VERSION=//p' $DOKP)
-TAG=cuda:$VERSION-runtimeplus-rockylinux9
-SRC=https://github.com/simoncblyth/sandbox/blob/master/$DOK
 
-vv="BASH_SOURCE PWD NAM DOK DOKP VERSION TAG SRC"
+#BASIS="cuda"     ## based off nvidia/cuda
+BASIS="junosw"  ## based off junosw/base:el9-9.4
+
+case $BASIS in
+  cuda)   DOK=junosw/Dockerfile-junosw-cudaxx-runtimeplus-el9 ;;
+  junosw) DOK=junosw/Dockerfile-from-junosw-cudaxx-runtimeplus-el9 ;;
+esac
+
+SRC=https://github.com/simoncblyth/sandbox/blob/master/$DOK
+DOKP=$(realpath ../$DOK)
+CUDA_VERSION=$(sed -n 's/^ARG CUDA_VERSION=//p' $DOKP)  ## eg 13.1.2
+CUDA_VERS=$(echo "${CUDA_VERSION}" | cut -d. -f1-2 )    ## eg 13.1
+
+
+case $BASIS in
+  cuda)   TAG=cuda:$CUDA_VERS-runtimeplus-rockylinux9   ;;
+  junosw) TAG=junosw:el9.4-cuda$CUDA_VERS-opticks    ;;
+esac
+
+
+NAM=simoncblyth
+
+vv="BASH_SOURCE PWD NAM DOK DOKP CUDA_VERSION CUDA_VERS TAG SRC"
 
 #defarg="info_build"
 defarg="info"
@@ -17,19 +57,30 @@ arg=${1:-$defarg}
 
 if [[ "$arg" =~ info ]]; then
    for v in $vv ; do printf "%30s : %s\n" "$v" "${!v}" ; done
-fi 
+   printf "%30s : %s\n" "NAM/TAG" "$NAM/$TAG"
+fi
 
 if [[ "$arg" =~ build ]]; then
    docker build \
       --pull=false \
-      --tag $NAM/$TAG --label "src=$SRC" --platform linux/amd64 - < $DOKP
-   [ $? -ne 0 ] && echo $BASH_SOURCE - ERROR from build && exit 1 
+      --label "org.opencontainers.image.revision=$(git rev-parse HEAD)" \
+      --label "org.opencontainers.image.created=$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+      --label "org.opencontainers.image.source=$SRC" \
+      --tag $NAM/$TAG \
+      --platform linux/amd64 - < $DOKP
+   [ $? -ne 0 ] && echo $BASH_SOURCE - ERROR from build && exit 1
+fi
+
+
+if [[ "$arg" =! inspect ]]; then
+   docker inspect --format='{{json .Config.Labels}}' $NAM/$TAG | jq   # jq pretty prints json
+   [ $? -ne 0 ] && echo $BASH_SOURCE - ERROR from inspect && exit 1
 fi
 
 
 if [[ "$arg" =~ look ]]; then
     docker run --gpus all -it --rm $NAM/$TAG /bin/bash
-   [ $? -ne 0 ] && echo $BASH_SOURCE - ERROR from look && exit 1 
+   [ $? -ne 0 ] && echo $BASH_SOURCE - ERROR from look && exit 1
 fi
 
 
